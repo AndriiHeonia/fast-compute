a7904336586ef52d951ae249ca6094523f132ced
AndreyGeonya <a.geonya@gmail.com>
2014-12-13 05:08:15 +0200

    webcl kernels to js functions binding

--- src/fast-compute.js
+++ src/fast-compute.js
@@ -15,81 +15,88 @@
     }
 
     // (String) -> Array
-    function _argInfo(src) {
-        var cType2jsType = {
-                // cType:   [jsType, isPointer]
-                'ushort':   ['Uint16Array', false],
-                'short':    ['Int16Array', false],
-                'int':      ['Int32Array', false],
-                'uint':     ['Uint32Array', false],
-                'float':    ['Float32Array', false],
-                'double':   ['Float64Array', false],
-                'ushort*':  ['Uint16Array', true],
-                'short*':   ['Int16Array', true],
-                'int*':     ['Int32Array', true],
-                'uint*':    ['Uint32Array', true],
-                'float*':   ['Float32Array', true],
-                'double*':  ['Float64Array', true]
-            },
-            argTypes = /kernel.*\((.*)\)/.exec(src)[1].split(',');
+    function _argsInfo(src) {
+        var argTypes = /kernel.*\((.*)\)/.exec(src)[1].split(',');
 
         argTypes = argTypes.map(function(arg) {
-            // TODO: support u?char\*?
-            var rexp = /(u?short\*?|u?int\*?|u?long\*?|float\*?|double\*?)/;
-            return rexp.exec(arg)[1];
-        });
-
-        argTypes = argTypes.map(function(cType) {
-            return cType2jsType[cType];
+            var rexp = /(u?char\*|u?short\*?|u?int\*?|u?long\*?|float\*?|double\*?)/,
+                argType = rexp.exec(arg)[1];
+            return {
+                cType: argType,
+                pointer: argType.indexOf('*') > -1
+            }
         });
 
         return argTypes;
     }
 
-    function Fast(src) {
-        var program, device;
+    // (String) -> Array
+    function _funcsInfo(src) {
+        var rexp = /kernel\s+void\s*(.*)\(.*\)/g,
+            match, obj, res = [];
+            
+        while (match = rexp.exec(src)) {
+            obj = {
+                str: match[0],
+                name: match[1],
+                argsInfo: _argsInfo(match[0])
+            }
+            res.push(obj);
+        }
 
+        return res;
+    }
+
+    function Fast(src) {
         _checkWebCl();
 
-        this.argInfo = _argInfo(src);
-        this.ctx = env.webcl.createContext();
+        // js methods according to WebCL kernels
+        _funcsInfo(src).forEach(function(fInfo) {
+            Fast.prototype[fInfo.name] = function() {
+                var args = Array.prototype.slice.call(arguments);
+                this.compute.apply(this, args.concat([fInfo]));
+            }
+        });
 
+        this.ctx = env.webcl.createContext();
         this.program = this.ctx.createProgram(src);
         this.device = this.ctx.getInfo(WebCL.CONTEXT_DEVICES)[0];
         this.program.build([this.device], '');
-
-        this.kernel = this.program.createKernel("callback");
-        this.cmdQueue = this.ctx.createCommandQueue(this.device);
     }
 
     Fast.prototype = {
         compute: function() {
-            var isPointer, memMode, buf, typedArray, globalWS;
+            var argInfo, memMode, buf, typedArray, globalWS,
+                funcInfo = arguments[arguments.length-1],
+                kernel = this.program.createKernel(funcInfo.name),
+                cmdQueue = this.ctx.createCommandQueue(this.device);
 
-            // set arguments to the kernels. Last argument - output
-            for (var i = 0; i < arguments.length; i++) {
-                isPointer = this.argInfo[i][1];
+            // set arguments to the kernels.
+            // arguments[arguments.length-1] - argInfo
+            // arguments[arguments.length-2] - output
+            for (var i = 0; i < arguments.length-1; i++) {
+                argInfo = funcInfo.argsInfo[i];
                 typedArray = arguments[i];
 
-                if (isPointer) {
-                    memMode = i < arguments.length - 1 ? WebCL.MEM_READ_ONLY : WebCL.MEM_WRITE_ONLY;
+                if (argInfo.pointer) {
+                    memMode = i < arguments.length-2 ? WebCL.MEM_READ_ONLY : WebCL.MEM_WRITE_ONLY;
                     buf = this.ctx.createBuffer(memMode, typedArray.byteLength);
-                    this.kernel.setArg(i, buf);
+                    kernel.setArg(i, buf);
                     if (memMode === WebCL.MEM_READ_ONLY) {
-                        this.cmdQueue.enqueueWriteBuffer(buf, false, 0, typedArray.byteLength, typedArray);                        
+                        cmdQueue.enqueueWriteBuffer(buf, false, 0, typedArray.byteLength, typedArray);                        
                     }
                 } else {
-                    this.kernel.setArg(i, typedArray);
+                    kernel.setArg(i, typedArray);
                 }
             }
 
             // init ND-range & exec
-            globalWS = this.kernel.getWorkGroupInfo(this.device, WebCL.KERNEL_WORK_GROUP_SIZE);
-            this.cmdQueue.enqueueNDRangeKernel(this.kernel, 1, null, [globalWS], null);
+            globalWS = kernel.getWorkGroupInfo(this.device, WebCL.KERNEL_WORK_GROUP_SIZE);
+            cmdQueue.enqueueNDRangeKernel(kernel, 1, null, [globalWS], null);
 
             // read result from OpenCL device to output typed array
-            this.cmdQueue.enqueueReadBuffer(buf, false, 0, typedArray.byteLength, typedArray);        
-            this.cmdQueue.finish();
+            cmdQueue.enqueueReadBuffer(buf, false, 0, typedArray.byteLength, typedArray);        
+            cmdQueue.finish();
 
             return typedArray;
         }
